116ae3a23363a26644d071029f86270025d2a9b3
Casey Tucker <dctucker@hotmail.com>
2024-09-23 23:25:55 -0700

    shift+dblquote is pipe

--- keyboards/ergodox_ez/keymaps/dctucker/keymap.c
+++ keyboards/ergodox_ez/keymaps/dctucker/keymap.c
@@ -29,7 +29,7 @@ enum custom_keycodes {
   COMMENT,
   BACKN,
   BARS,
-  MONEYQ,
+  PIPEQ,
   CTLESC,
   CTLENT,
   TAPMNBS,
@@ -57,7 +57,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_ergodox_pretty(
 		KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , LT1SIX ,  LT2FIVE, KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS,
-		KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_EQL ,  KC_BSLS, KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , MONEYQ ,
+		KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_EQL ,  KC_BSLS, KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , PIPEQ  ,
 		CTLESC , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                    KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
 		KC_LSFT, KC_Z   , KC_COMM, KC_X   , KC_C   , KC_V   , KC_B   ,  MOUSE1 , KC_B   , KC_N   , KC_M   , KC_DOT , KC_SLSH, RSHL4  ,
 		KC_DEL , KC_F1  , KC_PPLS, KC_LALT, KC_LGUI,                                      KC_RGUI, KC_RALT, KC_PMNS, KC_LBRC, KC_RBRC,
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[3] = LAYOUT_ergodox_pretty(
 		_______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,  KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,
 		_______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, _______,  KC_PIPE, _______, _______, _______, _______, _______, KC_F12 ,
-		KC_ESC , _______, _______, _______, _______, _______,                    KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_COLN, MONEYQ ,
+		KC_ESC , _______, _______, _______, _______, _______,                    KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_COLN, PIPEQ  ,
 		_______, _______, _______, _______, _______, _______, _______,  _______, _______, BACKN  , EARROW , ARROW  , COMMENT, _______,
 		_______, _______, _______, _______, _______,                                      _______, _______, _______, KC_LPRN, KC_RPRN,
 		                                             _______, _______,  _______, _______,
@@ -144,9 +144,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 				ctl_xtra_sent = false;
 				rctl_timer = timer_read();
 				register_code(KC_RCTL);
-				//layer_on(4);
 			} else {
-				//layer_off(4);
 				unregister_code(KC_RCTL);
 				if( ! ctl_xtra_sent ){
 					if( timer_elapsed(rctl_timer) < 500 ){
@@ -199,21 +197,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 			}
 			return false;
 			break;
-		case MONEYQ:
+		case PIPEQ:
 			xtra_sent = true;
 			if( record->event.pressed ){
 				uint8_t mods = get_mods();
 				add_key(KC_RSFT);
 				send_keyboard_report();
 				if( mods & MOD_BIT(KC_LSFT) ){
-					add_key(KC_4);
+					add_key(KC_BSLS);
 				} else {
 					add_key(KC_QUOTE);
 				}
 				send_keyboard_report();
 			} else {
 				del_key(KC_RSFT);
-				del_key(KC_4);
+				del_key(KC_BSLS);
 				del_key(KC_QUOTE);
 				send_keyboard_report();
 			}
